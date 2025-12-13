fbf5befa37acd67ca1e82289c42c02839a20cfd5
Yogi Prasetya <yogi.prasetya@digitaltelkom.id>
2025-12-13 17:03:01 +0700

    change nabvar to menubar

--- src/components/layout/topbar.tsx
+++ src/components/layout/topbar.tsx
@@ -1,69 +1,90 @@
-import { Cloud, CloudCheck } from "lucide-react";
+import { Cloud, CloudCheck, SidebarIcon } from "lucide-react";
 import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-} from "~/components/ui/navigation-menu";
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarTrigger,
+} from "~/components/ui/menubar";
 import { useNotes } from "~/services/use-notes";
 import { ThemeSetting } from "./theme-setting";
 import { useEditorStore } from "~/stores/editor-store";
 import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
+import { Button } from "../ui/button";
+import { useSidebar } from "../ui/sidebar";
 
 export function Topbar() {
   const { syncStatus } = useEditorStore();
   const { handleCreateNewNote } = useNotes();
+  const { toggleSidebar } = useSidebar();
 
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/50 bg-background/95 backdrop-blur-md supports-backdrop-filter:bg-background/80">
-      <div className="flex h-12 items-center justify-between gap-4 px-6">
+      <div className="flex h-12 items-center justify-between gap-4">
         <div className="flex items-center justify-between w-full">
           {/* Left: Workspace title | toolbar (new notes) */}
-          <NavigationMenu>
-            <NavigationMenuList className="flex-wrap">
-              <NavigationMenuItem>
-                <NavigationMenuTrigger>File</NavigationMenuTrigger>
+          <div className="w-64 flex items-center justify-between">
+            <p></p>
 
-                <NavigationMenuContent>
-                  <ul className="grid w-44 gap-2">
-                    <li>
-                      <NavigationMenuLink asChild>
-                        <button onClick={() => handleCreateNewNote(null)}>
-                          New Text Note
-                        </button>
-                      </NavigationMenuLink>
-
-                      <NavigationMenuLink asChild>
-                        <button onClick={() => handleCreateNewNote(null)}>
-                          New Folder Collection
-                        </button>
-                      </NavigationMenuLink>
-                    </li>
-                  </ul>
-                </NavigationMenuContent>
-              </NavigationMenuItem>
-            </NavigationMenuList>
-          </NavigationMenu>
+            <Button size="icon" variant="ghost" onClick={toggleSidebar}>
+              <SidebarIcon className="size-4" />
+              <span className="sr-only">Toggle sidebar</span>
+            </Button>
+          </div>
 
           {/* Right: Sync button and sync alert */}
-          <div className="flex items-center gap-2">
-            <Tooltip open={syncStatus === "saving"}>
-              <TooltipTrigger disabled asChild>
-                {syncStatus === "saving" ? (
-                  <Cloud className="size-4.5 animate-pulse" />
-                ) : (
-                  <CloudCheck className="size-4" />
-                )}
-              </TooltipTrigger>
+          <div className="flex flex-1 items-center justify-between px-4">
+            <Menubar>
+              <MenubarMenu>
+                <MenubarTrigger>File</MenubarTrigger>
 
-              <TooltipContent align="center" side="right">
-                Saving...
-              </TooltipContent>
-            </Tooltip>
+                <MenubarContent>
+                  <MenubarItem onClick={() => handleCreateNewNote(null)}>
+                    New Text Note
+                  </MenubarItem>
 
-            <ThemeSetting />
+                  <MenubarItem onClick={() => handleCreateNewNote(null)}>
+                    New Folder Collection
+                  </MenubarItem>
+                </MenubarContent>
+              </MenubarMenu>
+
+              <MenubarMenu>
+                <MenubarTrigger>Backup</MenubarTrigger>
+
+                <MenubarContent>
+                  <MenubarItem onClick={() => handleCreateNewNote(null)}>
+                    Export to MD
+                  </MenubarItem>
+
+                  <MenubarItem onClick={() => handleCreateNewNote(null)}>
+                    Export to JSON
+                  </MenubarItem>
+                </MenubarContent>
+              </MenubarMenu>
+
+              <MenubarMenu>
+                <MenubarTrigger>Setting</MenubarTrigger>
+              </MenubarMenu>
+            </Menubar>
+
+            <div className="flex items-center gap-2">
+              <Tooltip open={syncStatus === "saving"}>
+                <TooltipTrigger disabled asChild>
+                  {syncStatus === "saving" ? (
+                    <Cloud className="size-4.5 animate-pulse" />
+                  ) : (
+                    <CloudCheck className="size-4" />
+                  )}
+                </TooltipTrigger>
+
+                <TooltipContent align="center" side="right">
+                  Saving...
+                </TooltipContent>
+              </Tooltip>
+
+              <ThemeSetting />
+            </div>
           </div>
         </div>
       </div>
